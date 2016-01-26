1a188083a3f6fcb9a1bc7fa3c8101d568c02fb8c
Kiiyanatz <kiiyanatz@gmail.com>
2016-01-26 16:06:37 +0300

    message

--- str_reverse.py
+++ str_reverse.py
@@ -1,4 +1,12 @@
 def reverse(s):
 	return s[::-1]
-print reverse("andela")
-print reverse("hello")
+
+def reversex(x):
+	x = list(x)
+	for i in range(len(x)//2):
+		temp = x[i] # Holds letter n
+		x[i] = x[len(x) - 1 -i] # first index = last index
+		x[len(x) - 1 -i]= temp
+	return x
+
+print " ".join(reversex("hello"))
