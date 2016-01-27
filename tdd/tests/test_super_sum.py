d3df446f4a3e4f082eb42fdd0a705e0a39250689
Kiiyanatz <kiiyanatz@gmail.com>
2016-01-27 16:58:35 +0300

    Updated changes

--- tdd/tests/test_super_sum.py
+++ tdd/tests/test_super_sum.py
@@ -5,3 +5,28 @@ class SuperSumTest(unittest.TestCase):
     def test_two_numbers(self):
         result = super_sum(20, 20)
         self.assertEqual(result, 40)
+
+    def test_four_numbers(self):
+    	result = super_sum([20, 40, 50, 60])
+    	self.assertEqual(result, 170)
+
+    def test_two_lists(self):
+    	a = [10, 20, 30, 40]
+    	b = [100, 20]
+    	result = super_sum(a, b)
+    	self.assertEqual(result, 220)
+
+    def test_list_and_number(self):
+    	a = [10, 30, 50]
+    	result = super_sum(a, 50)
+    	self.assertEqual(result, 140)
+
+    def test_four_lists(self):
+    	a, b, c, d = [1, 2], [2, 3], [3], [4, 10]
+    	result = super_sum(a, b, c, d)
+    	self.assertEqual(result, 25)
+
+    #def test_large_input(self):
+    #	a = [i**3 for i in xrange(100)]
+
+
